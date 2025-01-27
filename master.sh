0622377a34634de4ff297688ce564ba1019afd4e
AsaTyr <43162495+AsaTyr2018@users.noreply.github.com>
2025-01-27 08:45:38 +0100

    Update master.sh
    Added missing Task 6 Transfer to Output/Cleanup

--- master.sh
+++ master.sh
@@ -65,4 +65,7 @@ run_shell_task "Transfer to Quality Check" "${SCRIPT_DIR}/transfer-face-quali.sh
 # Task 5: Quality Control
 run_python_task "Quality Check" "${SCRIPT_DIR}/quality_check.py" "${SCRIPT_DIR}/task_running_QualityCheck"
 
+# Task 6: Transfer (QUALI -> output)
+run_shell_task "Transfer to Output and Cleanup" "${SCRIPT_DIR}/transfer-output.sh" "${SCRIPT_DIR}/task_running_TransferQualiToOutput"
+
 echo "All tasks completed. Master script finished."
